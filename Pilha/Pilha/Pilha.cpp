f24b9a783e76e6af3e3eda30b3c50e961e94a21a
EricUnderOcean <underoceanguy@gmail.com>
2024-10-01 11:36:00 -0300

    Update Pilha.cpp

--- Pilha/Pilha/Pilha.cpp
+++ Pilha/Pilha/Pilha.cpp
@@ -59,7 +59,7 @@ void menu()
 void inicializar()
 {
 
-	// se a lista j· possuir elementos
+	// se a lista j√° possuir elementos
 	// libera a memoria ocupada
 	NO* aux = topo;
 	while (aux != NULL) {
@@ -104,7 +104,7 @@ void pop()
 	NO* aux = topo;
 	if (topo == NULL) 
 	{
-		cout << "A Lista est· vazia \n";
+		cout << "A Pilha est√° vazia \n";
 		return;
 	}
 	else if (topo->prox != NULL)
